455342efcdce8ca9f716e85eb70c45e7a80b011a
Alex7777 <934956752@qq.com>
2022-07-29 08:30:28 +0800

    mockjs env preparation for ChartView

---
+++ src/components/ChartView.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <el-col :span="18" class="mt-5">
+      <el-card style="height: 200px">123</el-card>
+    </el-col>
+    <el-col class="mt-5" :span="9">
+      <el-card style="height: 235px">123</el-card>
+    </el-col>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from "vue-property-decorator";
+
+@Component
+export default class ChartView extends Vue {}
+</script>
+
+<style scoped></style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -2,7 +2,7 @@
   <el-row :gutter="15">
     <el-col :span="6">
       <el-card class="mt-5" shadow="hover">
-        <div class="flex justify-start gap-5 m-3 items-center" slot="header">
+        <div class="flex justify-start gap-5 items-center" slot="header">
           <img
             :src="imgUrl"
             class="w-20 h-20 rounded-full bg-red-300"
@@ -13,7 +13,7 @@
             <p class="text-gray-400 py-2 text-base">超级管理员</p>
           </div>
         </div>
-        <div class="footer border-0">
+        <div class="footer p-2 pl-4 border-0">
           <p class="text-gray-400 pl-3 m-0 text-sm">
             IP地址: {{ ipInfo.ip_address }}
           </p>
@@ -27,6 +27,7 @@
       </el-card>
     </el-col>
     <OrderOverview />
+    <ChartView />
   </el-row>
 </template>
 
@@ -35,11 +36,13 @@ import axios from "axios";
 import { Component, Vue } from "vue-property-decorator";
 import SalesOverview from "@/components/SalesOverview.vue";
 import OrderOverview from "@/components/OrderOverview.vue";
+import ChartView from "@/components/ChartView.vue";
 
 @Component({
   components: {
     SalesOverview,
     OrderOverview,
+    ChartView,
   },
 })
 export default class HomeView extends Vue {
@@ -47,7 +50,7 @@ export default class HomeView extends Vue {
   imgUrl = require("@/assets/peep-1.png");
   ipInfo = {};
 
-  mounted() {
+  created() {
     axios
       .get(
         "https://ipgeolocation.abstractapi.com/v1/?api_key=d2448e6f3e954983b3edb8c7f8944eae"
