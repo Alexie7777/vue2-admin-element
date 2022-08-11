5bd95d41d3b046b9cc9248be9fa27499372ddcbb
Alex7777 <934956752@qq.com>
2022-08-11 12:08:57 +0800

    🚀 cache HomeView

--- src/views/MainView.vue
+++ src/views/MainView.vue
@@ -9,7 +9,9 @@
           <CommonHeader />
         </el-header>
         <el-main>
-          <router-view></router-view>
+          <keep-alive include="HomeView">
+            <router-view></router-view>
+          </keep-alive>
         </el-main>
       </el-container>
     </el-container>
